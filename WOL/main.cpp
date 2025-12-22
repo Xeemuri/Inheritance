ec649ed35bdfb9ac06d06d080a5daec1c5f168e6
Илья Щербаков <iliya309@mail.ru>
2025-12-22 14:29:37 +1000

    201 WOL and 201.dhcpd DONE

--- WOL/main.cpp
+++ WOL/main.cpp
@@ -7,17 +7,25 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	std::ifstream fin("201 RAW.txt");
-	std::ofstream fout("201 WOL.txt");
+	std::ofstream fout_wol("201 WOL.txt");
+	std::ofstream fout_dhcpd("201.dhcpd.txt");
 	const int SIZE = 20;
 	char ip_buffer[SIZE] = {};
 	char mac_buffer[SIZE] = {};
+	int cnt = 1;
 	if (fin.is_open())
 	{
 		while (!fin.eof())
 		{
 			fin >> ip_buffer;
 			fin >> mac_buffer;
-			fout << mac_buffer << "\t\t" << ip_buffer << endl;
+			fout_wol << mac_buffer << "\t\t" << ip_buffer << endl;
+
+			fout_dhcpd << "host-" << cnt++ << endl;
+			fout_dhcpd << "{" << endl;
+			fout_dhcpd << "\thardware ethernet\t" << mac_buffer << endl;
+			fout_dhcpd << "\tfixed-address\t" << ip_buffer << endl;
+			fout_dhcpd << "}" << endl << endl;
 		}
 	}
 	else
@@ -25,5 +33,5 @@ void main()
 		std::cerr << "Error: file not found." << endl;
 	}
 	fin.close();
-	fout.close();
+	fout_wol.close();
 }
