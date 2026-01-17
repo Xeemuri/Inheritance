35f4bc2b828140ad9459906ee91c269f859446de
Илья Щербаков <iliya309@mail.ru>
2026-01-18 02:37:55 +1000

    Class Circle DONE Ternary operators in set_methods DONE

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 using namespace std;
 
+const double PI = 3.1415926535;
+
 enum Color
 {
 	Black = 0x00000000,
@@ -16,7 +18,7 @@ enum Color
 
 #define SHAPE_TAKE_PARAMETERS int start_x, int start_y, int line_width, Color color
 #define SHAPE_GIVE_PARAMETERS start_x, start_y, line_width, color
-		
+
 class Shape
 {
 	static const int MIN_START_X = 100;
@@ -46,21 +48,21 @@ public:
 
 	void set_start_x(int start_x)
 	{
-		if (start_x < MIN_START_X) start_x = MIN_START_X;
-		if (start_x > MAX_START_X) start_x = MAX_START_X;
-		this->start_x = start_x;
+		//if (start_x < MIN_START_X) start_x = MIN_START_X;
+		//if (start_x > MAX_START_X) start_x = MAX_START_X;
+		this->start_x = start_x < MIN_START_X ? MIN_START_X : start_x > MAX_START_X ? MAX_START_X : start_x;
 	}
 	void set_start_y(int start_y)
 	{
-		if (start_y < MIN_START_Y) start_y = MIN_START_Y;
-		if (start_y > MAX_START_Y) start_y = MAX_START_Y;
-		this->start_y = start_y;
+		//if (start_y < MIN_START_Y) start_y = MIN_START_Y;
+		//if (start_y > MAX_START_Y) start_y = MAX_START_Y;
+		this->start_y = start_y < MIN_START_Y ? MIN_START_Y : start_y > MAX_START_Y ? MAX_START_Y : start_y;
 	}
 	void set_line_width(int line_width)
 	{
-		if (line_width < MIN_LINE_WIDTH) line_width = MIN_LINE_WIDTH;
-		if (line_width > MAX_LINE_WIDTH)line_width = MAX_LINE_WIDTH;
-		this->line_width = line_width;
+		//if (line_width < MIN_LINE_WIDTH) line_width = MIN_LINE_WIDTH;
+		//if (line_width > MAX_LINE_WIDTH)line_width = MAX_LINE_WIDTH;
+		this->line_width = line_width < MIN_LINE_WIDTH? MIN_LINE_WIDTH: line_width > MAX_LINE_WIDTH? MAX_LINE_WIDTH: line_width;
 	}
 	double filter_size(double size)
 	{
@@ -76,7 +78,7 @@ public:
 	}
 	virtual ~Shape() {}
 
-	
+
 	virtual double get_area() const = 0;
 	virtual double get_perimeter() const = 0;
 	virtual void draw() const = 0;
@@ -196,10 +198,10 @@ public:
 		this->width = width;
 	}
 
-	Rectangle(int height, int width, int start_x, int start_y, int line_width, Color color) :Shape(start_x, start_y, line_width, color)
+	Rectangle(int height, int width, SHAPE_TAKE_PARAMETERS) :Shape(SHAPE_GIVE_PARAMETERS)
 	{
-		this->height = height;
-		this->width = width;
+		this->height = filter_size(height);
+		this->width = filter_size(width);
 	}
 	~Rectangle() {}
 
@@ -214,7 +216,7 @@ public:
 
 
 		//3) Создаем чем мы будем рисовать
-		HPEN hPen = CreatePen(PS_SOLID, 5, color); // Карандаш - рисует контур фигуры
+		HPEN hPen = CreatePen(PS_SOLID, line_width, color); // Карандаш - рисует контур фигуры
 		HBRUSH hBrush = CreateSolidBrush(color);	//Кисть рисует заливку фигуры
 
 		//4) Вышесозданные инструменты нужно выбрать (взять в руки):
@@ -222,8 +224,7 @@ public:
 		SelectObject(hdc, hBrush);
 
 		//5)Рисуем фигуру:
-		const int indent = 200;
-		::Rectangle(hdc, indent, indent, width + indent, height + indent);
+		::Rectangle(hdc, start_x, start_y, start_x + width, start_y + height);
 
 
 		//6) Удаляем инструменты, для того чтобы освободить ресурсы, занимаемые этими инструментами:
@@ -231,17 +232,79 @@ public:
 		DeleteObject(hBrush);
 		ReleaseDC(hwnd, hdc);
 	}
+	void info() const override
+	{
+		cout << typeid(*this).name() << endl;
+		cout << "Ширина: " << get_width() << endl;
+		cout << "Высота: " << get_height() << endl;
+		Shape::info();
+	}
+};
+
+class Circle :public Shape
+{
+	double radius;
+public:
+	double get_radius() const
+	{
+		return radius;
+	}
+	void set_radius(double radius)
+	{
+		this->radius = filter_size(radius);
+	}
+
+	Circle(double radius, SHAPE_TAKE_PARAMETERS) :Shape(SHAPE_GIVE_PARAMETERS)
+	{
+		set_radius(radius);
+	}
+	~Circle() {}
+
+	double get_area() const override { return PI * radius * radius; }
+	double get_perimeter() const override { return 2 * PI * radius; }
+
+
+	void draw() const override
+	{
+		HWND hwnd = GetConsoleWindow(); //1)Получаем окно консоли, чтобы к нему можно было подключаться
+		HDC hdc = GetDC(hwnd); //2)Получаем контекст окна консоли. Контекст - то, на чем мы будем рисовать.
+
+
+		//3) Создаем чем мы будем рисовать
+		HPEN hPen = CreatePen(PS_SOLID, line_width, color); // Карандаш - рисует контур фигуры
+		HBRUSH hBrush = CreateSolidBrush(color);	//Кисть рисует заливку фигуры
+
+		//4) Вышесозданные инструменты нужно выбрать (взять в руки):
+		SelectObject(hdc, hPen);
+		SelectObject(hdc, hBrush);
+
+		//5)Рисуем фигуру:
+
+		Ellipse(hdc, start_x, start_y, radius + start_x, radius + start_y);
+
+		//6) Удаляем инструменты, для того чтобы освободить ресурсы, занимаемые этими инструментами:
+		DeleteObject(hPen);
+		DeleteObject(hBrush);
+		ReleaseDC(hwnd, hdc);
+	}
+
+	void info() const override
+	{
+		cout << typeid(*this).name() << endl;
+		cout << "Радиус окружности: " << get_radius() << endl;
+		Shape::info();
+	}
 };
 int main()
 {
 	setlocale(LC_ALL, "");
-	//Shape shape = Color::Red;
-	Square square(50000, -300, -300, 1, Color::White);
-	/*cout << "Сторона квадрата: " << square.get_side() << endl;
-	cout << "Площадь фигуры: " << square.get_area() << endl;
-	cout << "Периметр фигуры: " << square.get_perimeter() << endl;
-	square.draw();*/
-	//class Rectangle rect(300, 300, Color::Purple);
-	square.info();
+
+	Square square(50000, 3200, -300, 1, Color::White);
+	//square.info();
+
+	class Rectangle rect(800, 600,100,200,5, Color::Purple);
 	//rect.info();
+
+	Circle circle(3000, 100, 100, 15, Blue);
+	//circle.info();
 }
